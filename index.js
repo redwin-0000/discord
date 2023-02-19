1d332d86b52d47b493c25527a33c28005f0e6a5e
redwin-0000 <redwin125896@gmail.com>
2023-02-19 11:46:34 +0530

    update some changes

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const sideMenu = document.querySelector(".side-menu");
+const sideMenubar = document.querySelector(".side-menu-bar");
 const body = document.querySelector("body");
 function menuToggler() {
-  if (sideMenu.classList.contains("translate-x-[650px]")) {
-    sideMenu.classList.remove("translate-x-[650px]");
-
-    sideMenu.classList.add("translate-x-0");
+  if (sideMenubar.classList.contains("translate-x-[650px]")) {
+    sideMenubar.classList.remove("translate-x-[650px]");
+    sideMenubar.classList.add("translate-x-0");
   } else {
-    sideMenu.classList.remove("translate-x-0");
-    sideMenu.classList.add("translate-x-[650px]");
+    sideMenubar.classList.remove("translate-x-0");
+    sideMenubar.classList.add("translate-x-[650px]");
   }
 }
